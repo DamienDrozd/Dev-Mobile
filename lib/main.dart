59fb4d64ff887786fe9fdba61140421527b7f81e
KevOneRedOne <kevinalves@hotmail.fr>
2022-03-30 11:01:50 +0200

    Add contact, messenger and profil page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 // ignore_for_file: avoid_print
-
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:tpfinal/functions/firebaseHelper.dart';
