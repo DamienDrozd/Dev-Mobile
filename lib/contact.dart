69ca29507b0a1fd8d30ee509e501846de8b06010
KevOneRedOne <kevinalves@hotmail.fr>
2022-03-31 08:55:36 +0200

    update main, messenger, profil

---
+++ lib/contact.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: camel_case_types
+
+import 'package:flutter/material.dart';
+
+class contact extends StatefulWidget {
+  const contact({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return contactState();
+  }
+}
+
+class contactState extends State<contact> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text("coucou");
+  }
+}
